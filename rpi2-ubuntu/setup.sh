a9e11744ef72b83f0d56997302841850faec853d
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-17 22:36:41 +0200

    up

--- rpi2-ubuntu/setup.sh
+++ rpi2-ubuntu/setup.sh
@@ -7,7 +7,14 @@ echo nameserver 1.1.1.1 > /etc/resolv.conf
 # Enable ssh
 touch /boot/ssh
 
+# Add keys
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
+
+# Add repos
+echo deb http://pkg.jenkins-ci.org/debian binary/ | tee /etc/apt/sources.list.d/jenkins.list
+
 # Packages installation/removal
 apt-get update
-apt-get install -y docker.io
+apt-get install -y docker.io jenkins openjdk-11-jdk-headless
 apt-get remove -y --purge snapd
+apt-get autoremove --purge
